fbca1be23e26540309371a674279169a57e6c828
Rubalruhaily <ruba.alruhaily@gmail.com>
2022-12-06 23:53:04 +0300

    Initial Commit

---
+++ Estafhm/EstafhmApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstafhmApp.swift
+//  Estafhm
+//
+//  Created by Ruba on 12/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct EstafhmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
