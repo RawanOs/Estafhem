2603ab9e594947cd044f0c4e53ad0f50d357a3aa
Rawan <roalsufyani22@twq.idserve.net>
2022-12-08 09:34:52 +0300

    Initial Commit

---
+++ Estafhem/EstafhemApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstafhemApp.swift
+//  Estafhem
+//
+//  Created by Rawan on 14/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct EstafhemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
