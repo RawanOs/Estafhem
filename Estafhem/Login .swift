78485d4a3dd3107de6c2e5eb60c0e8d35cc84bd2
Rawan <roalsufyani22@twq.idserve.net>
2022-12-12 12:51:57 +0300

    c2

---
+++ Estafhem/Login .swift
@@ -0,0 +1,24 @@
+//
+//  Log in .swift
+//  Estafhem
+//
+//  Created by Rawan on 14/05/1444 AH.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+struct Login: View {
+    var body: some View {
+        Text("TEST")
+        
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
